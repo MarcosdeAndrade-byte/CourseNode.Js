30a205402049ca560cf13311b9569ec6bf1c956a
Marcos Alexandre <junior.marcos.mjr@hotmail.com>
2022-04-28 15:24:06 -0300

    modules e require

---
+++ people.js
@@ -0,0 +1,10 @@
+//Para exportar blocos de código temos que seguir o padrão abaixo
+//utilizando module.exports
+const people = ['Marcos',"Ryu","Chun-li","Mario"];
+const ages = [20,25,30,35];
+
+
+//Podemos exportar variáveis,objetos,funções e etc...
+module.exports = {
+    people,ages
+}
