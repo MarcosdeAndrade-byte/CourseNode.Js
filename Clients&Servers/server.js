10d26c0f1d55612f5a5b9517a77b81e2c4a2baad
Marcos Alexandre <junior.marcos.mjr@hotmail.com>
2022-05-01 15:16:56 -0300

    creating a server

--- Clients&Servers/server.js
+++ Clients&Servers/server.js
@@ -8,3 +8,18 @@
 
 //Diferente de outras linguagens em Node criamos o servidor do absoluto 
 //zero.
+
+//Importamos o módulo http para criar um servidor
+const http = require('http');
+
+//Armazenamos uma instância do nosso servidor na constante server
+//o método createServer cria um servidor
+//O método req vem carregado de informações sobre as requisições 
+//O método res é utilizado para enviar respostas ao usuário
+const server = http.createServer((req,res) => {
+    console.log("Requisições feitas");
+});
+
+server.listen(3000,'localhost',() => {
+    console.log('Ouvindo as requisições na porta 3000');
+});
