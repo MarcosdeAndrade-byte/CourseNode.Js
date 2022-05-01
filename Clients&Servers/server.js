bc43ae38a626683a3645e0352be52d8a6ee147aa
Marcos Alexandre <junior.marcos.mjr@hotmail.com>
2022-05-01 14:57:34 -0300

    clients & servers_ip addresses&domains

---
+++ Clients&Servers/server.js
@@ -0,0 +1,10 @@
+//Um servidor tem como objetivo atender as solicitações recebidas
+//do navegador,ele pode retornar uma página HTML,JSON,css e etc...
+
+//Os computadores especiais que hospedam sites na internet recebem
+//o nome de host e tem um IP para identifica-los, para se conectar
+//a qualquer site devemos escrever seu endereço IP,ou de forma mais
+//simples,podemos apenas digitar o domínio associado ao site.
+
+//Diferente de outras linguagens em Node criamos o servidor do absoluto 
+//zero.
