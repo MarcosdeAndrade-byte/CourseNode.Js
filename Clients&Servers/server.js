7194dc19b67c2c62ca49e355a95d1c8369f2d5f5
Marcos Alexandre <junior.marcos.mjr@hotmail.com>
2022-05-02 11:55:57 -0300

    Response object

--- Clients&Servers/server.js
+++ Clients&Servers/server.js
@@ -1,31 +1,19 @@
-//Um servidor tem como objetivo atender as solicitações recebidas
-//do navegador,ele pode retornar uma página HTML,JSON,css e etc...
-
-//Os computadores especiais que hospedam sites na internet recebem
-//o nome de host e tem um IP para identifica-los, para se conectar
-//a qualquer site devemos escrever seu endereço IP,ou de forma mais
-//simples,podemos apenas digitar o domínio associado ao site.
-
-//Diferente de outras linguagens em Node criamos o servidor do absoluto 
-//zero.
-
-//Importamos o módulo http para criar um servidor
 const http = require('http');
 
-//Armazenamos uma instância do nosso servidor na constante server
-//o método createServer cria um servidor
-//O método req vem carregado de informações sobre as requisições 
-//O método res é utilizado para enviar respostas ao usuário
+//O objeto req vem carregado de informações sobre as requisições e podemos utilizar isso
 const server = http.createServer((req,res) => {
-    console.log("Requisições feitas");
+    //O exemplo abaixo retorna a /url e o método de requisição que fazemos ao servidor
+    console.log(req.url,req.method);
+
+    //define o tipo de conteúdo cabeçalho 
+    res.setHeader('Content-Type','text/html');
+    //Através do método write podemos retornar fragmentos de html,json,css e etc...(Existem maneiras melhores)
+    res.write('<p>Ola,ninjas</p>');
+    res.write('<h3>Ola novamente,ninjas</h3>');
+    //Avisamos para o navegador que terminamos de escrever a resposta
+    res.end();
 });
 
-//Quando nos referimos ao localhost estamos na verdade utilizando o nosso
-//próprio computador como um host para fazer requisições e desenvolver sites
 server.listen(3000,'localhost',() => {
-    //Port Numbers são utilizadas para se comunicar com o nosso servidor
-    //muitos aplicativos utilizam as portas para se comunicar e devemos 
-    //ficar atentos para não tentar estabelecer uma conexão com uma porta
-    //que está sendo utilizada (Padrão da porta para desenvolvimento: 3000)
     console.log('Ouvindo as requisições na porta 3000');
 });
