86c22016198fda7dd9d23789147497b0ea881fe3
Marcos Alexandre <junior.marcos.mjr@hotmail.com>
2022-05-03 14:43:09 -0300

    routing e html pages

--- expressApps/app.js
+++ expressApps/app.js
@@ -7,11 +7,15 @@ const app = express();
 // listen for requests
 app.listen(3000);
 
-//Ao invés de utilizar 
+//Ao invés de utilizar um try/catch podemos direcionar cada requisição para sua página utilizando o  Express
 app.get('/',(req,res)=>{
-    res.send("<h1>DEU CERTOOOOOOOOOOOOOOOOOOOOOOOOO</h1>");
+    res.sendFile('F:/WS-VSC/CourseNodeJs/Clients&Servers/views/index.html');
 });
 
+
 app.get('/about',(re,res)=>{
-    
+    //Através da função sendFile podemos retornar um arquivo do nosso diretório
+    res.sendFile('F:/WS-VSC/CourseNodeJs/Clients&Servers/views/about.html');
 });
+
+
