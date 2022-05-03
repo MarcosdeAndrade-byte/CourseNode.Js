2ab7071b2a4fd70893317f912599f39f414c53a6
Marcos Alexandre <junior.marcos.mjr@hotmail.com>
2022-05-03 10:34:05 -0300

    NPM

---
+++ npm/npm.js
@@ -0,0 +1,12 @@
+//Podemos dizer que o NPM é um gerenciador de dependências
+//nosso primeiro pacote externo vai ser o nodemon (Que recarrega 
+//automaticamente o servidor)
+
+//Comando para rodar adicionar o nodemon -> npm instal -g nodemon
+
+//Para criar um package.json do projeto devemos executar o seguinte 
+//comando: npm init 
+
+//Podemos baixar as dependências de forma local,global e como desenvolvedor
+
+//Não devemos subir a pasta de módulos para o GitHub
