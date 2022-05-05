63fd64e2dd07c89798a8b401831abfb6e971a674
Marcos Alexandre <junior.marcos.mjr@hotmail.com>
2022-05-05 09:22:10 -0300

    Utilizacao da ejs view engine_ organizacao de pastas_e utilizacao do nodemon

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
+console.log("SERVIDOR RODANDO NA PORTA 3000");
+
 //express retorna uma função
 const express = require('express');
 
 //express app
 const app = express();
 
+//register view engine (Vamos criar tamplates através do ejs para retornar dados do banco mais facilmente)
+// O app.set nos permite configurar algumas configurações do aplicativo
+app.set('view engine','ejs'); 
+
 // listen for requests
 app.listen(3000);
 
 //Ao invés de utilizar um try/catch podemos direcionar cada requisição para sua página utilizando o  Express
 app.get('/',(req,res)=>{
-    res.sendFile('F:/WS-VSC/CourseNodeJs/Clients&Servers/views/index.html');
+    res.render('index');
 });
 
 
 app.get('/about',(re,res)=>{
-    //Através da função sendFile podemos retornar um arquivo do nosso diretório
-    res.sendFile('F:/WS-VSC/CourseNodeJs/Clients&Servers/views/about.html');
+    res.render('about');
 });
 
-//Redirecionamento 
-app.get('/about-us',(req,res)=> {
-    //respondemos com outra página html
-    res.redirect('/about');
-});
 
 //A função use é utilizada para criar middleware e acionar funções de middleware no Express
 //Se nenhuma das requisições funcionar retornamos um erro 404
 app.use((req,res)=>{
-    res.status(404).sendFile('F:/WS-VSC/CourseNodeJs/Clients&Servers/views/404.html');
+    res.status(404).render('404');
 });
